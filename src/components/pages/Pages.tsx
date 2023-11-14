436f2e090cca0d01582c3c0f5af355e39682a4ea
Marcin.wrzs <marcin.wrzs@gmail.com>
2023-11-14 20:55:10 +0100

    added error page

--- src/components/pages/Pages.tsx
+++ src/components/pages/Pages.tsx
@@ -3,13 +3,15 @@ import Category from "pages/category/Category";
 import HomePage from "pages/HomePage/HomePage";
 import SignUp from "pages/singUp/SignUp";
 import { Route, Routes } from "react-router-dom";
-import * as Styled from "./Pages.Styled";
+import * as Styled from "./Pages.styled";
+import ErrorPage from "pages/ErrorPage/ErrorPage";
 
 export enum Paths {
   Home = "/",
   SignUp = "/signUp",
   SignIn = "/SignIn",
   Category = "/category/",
+  ErrorPage = "*",
 }
 
 const Pages: React.FC = () => {
@@ -20,6 +22,7 @@ const Pages: React.FC = () => {
         <Route path={Paths.SignUp} element={<SignUp />} />
         <Route path={Paths.SignIn} element={<SignIn />} />
         <Route path={Paths.Category + ":category"} element={<Category />} />
+        <Route path={Paths.ErrorPage} element={<ErrorPage />} />
       </Routes>
     </Styled.Wrapper>
   );
