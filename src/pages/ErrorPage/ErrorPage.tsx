6d75f2a97a0a76f03113d77f9bc1cb92508701b2
Marcin.wrzs <marcin.wrzs@gmail.com>
2023-11-16 20:26:13 +0100

    strapi update

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -3,7 +3,7 @@ import * as Styled from "./ErrorPage.styled";
 const ErrorPage: React.FC = () => {
   return (
     <Styled.Wrapper>
-      <h1>PAGE NOT FOUND.</h1>
+      <h1>PAGE NOT FOUND</h1>
     </Styled.Wrapper>
   );
 };
