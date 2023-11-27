32c2d09fce9d21bf3149e5821a1ec645dabc8e08
Marcin.wrzs <marcin.wrzs@gmail.com>
2023-11-27 22:28:08 +0100

    updated validation

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,13 +1,70 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+
+type SignInTypes = {
+  name: string;
+  email: string;
+  password: string;
+};
 
 const SignIn: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignInTypes>();
+
+  const onSubmit = () => {
+    console.log("submitted");
+  };
+
+  const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+
   return (
     <div>
-      SignIn
-      <p>mail</p>
-      <p>password</p>
-      <button>login</button>
-      <p>No account? Create one</p>
+      <h1>SignIn</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <label>
+          Name:
+          <input
+            type="text"
+            {...register("name", {
+              required: "Name is required",
+            })}
+          />
+          <p>{errors.name?.message}</p>
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            {...register("email", {
+              required: "This field cannot be empty",
+              pattern: {
+                value: emailRegex,
+                message: "Please use a valid email",
+              },
+            })}
+          />
+          <p>{errors.email?.message}</p>
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            {...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 6,
+                message: "Password must be at least 6 characters",
+              },
+            })}
+          />
+          <p>{errors.password?.message}</p>
+        </label>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 };
